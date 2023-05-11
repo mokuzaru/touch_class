1d94d2ed2b1cdcc31bcc16f84dd72751384fbb62
mokuzaru <kenny.otoya@pucp.pe>
2023-05-10 20:39:17 -0500

    first

---
+++ test1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void touch::Update()
+extern void touch_Update_m718329B76C674459F6AC91C5A65C5461E3D55EEF (void);
+// 0x00000002 System.Void touch::.ctor()
+extern void touch__ctor_m8E6F22E649751DC7A0CE42FF2227673623A64968 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	touch_Update_m718329B76C674459F6AC91C5A65C5461E3D55EEF,
+	touch__ctor_m8E6F22E649751DC7A0CE42FF2227673623A64968,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	3420,
+	3420,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
